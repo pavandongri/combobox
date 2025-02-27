1e8c022d963f31a84bfae0cf2613dfa35c18ea44
Dongri  Pavan Kumar <dongri.pavan@jithvar.com>
2025-02-27 22:32:32 +0530

    commit-2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from "./redux/store"
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ProductDropdown from './components/ProductDropdown';
+import React from "react";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ProductDropdown from "./components/ProductDropdown";
 
 function App() {
   return (
@@ -18,7 +18,6 @@ function App() {
 
 export default App;
 
-
 /* ===============     Some Extra Features which I have added     =============================
 
   1) (dummyjson.com) this api renders unused data for each object in the api, which can cause performance issues, 
@@ -27,20 +26,21 @@ export default App;
 
   2) When we click outside of the dropdown, the dropdown closes
      And when we click on the searchbar then dropdown opens again
-     
-  3) Each time instead of fetching more data, I fetched only 10 records from the api, this increases the performance
 
-  4) When user enters search-word, instead of calling the apis immediately 
+  3) I have also implemented url-sharing, so that state of this app can we preserved when we open the url in new tab
+
+  4) This app is designed in fully responsive way 
+     
+  5) Each time instead of fetching more data, I fetched only 10 records from the api, this increases the performance
+
+  6) When user enters search-word, instead of calling the apis immediately 
     I used debounce technique, to call the api after 1 second of user entered the search-word
     This reduces the redundant api calls and increases performance drastically
     This saves the band-width, resources.
 
-  5) Maintained clean project structure
+  7) Maintained clean project structure
 
 */
-
-
-
 
 /* ===============     List limitations here     =============================
  
@@ -63,5 +63,3 @@ export default App;
   - we can add animation effects if needed
 
  */
-
-  
